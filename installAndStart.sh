94e469720787abcecce3b8ae70c0ba49d325e642
Nicolas Bös <nicolas.boes@nolteundlauth.de>
2015-04-22 08:44:51 +0200

    Check if gin is missing on start and install.

--- installAndStart.sh
+++ installAndStart.sh
@@ -3,4 +3,6 @@
 cd /go/src/app
 go-wrapper download
 go-wrapper install
+#check and install gin if missing i.e. go directory was overriden or mounted
+go get -v github.com/codegangsta/gin
 exec gin "$@"
